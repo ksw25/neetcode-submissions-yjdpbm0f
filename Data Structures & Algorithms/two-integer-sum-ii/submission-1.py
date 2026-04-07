166348b6e86c5c713669865f1beec41f66a47ddd
Karanpreet Singh Wadhwa <44509423+ksw25@users.noreply.github.com>
2026-04-07 01:18:04 -0400

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.py
@@ -0,0 +1,12 @@
+import bisect
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l,r= 0, len(numbers)-1
+        while l<r:
+            c=numbers[l] + numbers[r]
+            if c==target:
+                return [l+1,r+1]
+            elif c>target:
+                r-=1
+            else:
+                l+=1
